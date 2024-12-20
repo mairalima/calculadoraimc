4965b83d6f57c1936e20dff1df82eb9af3f74445
mairalima <maijucelima@gmail.com>
2024-12-20 11:54:35 -0300

    calculando o imc

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -16,10 +16,11 @@ class MainActivity : AppCompatActivity() {
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
         btnCalcular.setOnClickListener{
-            val peso= edtPeso.text
-            println("Maira " + peso)
-            val altura= edtAltura.text
-            println("Maira" + altura)
+            val peso: Float = edtPeso.text.toString().toFloat()
+            val altura: Float =  edtAltura.text.toString().toFloat()
+            val alturaQ2 = altura * altura
+            val resultado = peso / alturaQ2
+            println("Maira" + resultado)
         }
     }
 }
